90dc551345a0d0c1b3a59300153026f101cdfe13
Chamisew <chamilasewmini2@gmail.com>
2024-08-24 14:22:20 +0530

    build comment for undefine files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
+//import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import IncomeList from './pages/IncomeList'; 
-import EditIncome from './pages/EditIncome'; 
-import Home from './pages/Home'; 
-import GoalForm from './components/GoalForm';
-import GoalList from './components/GoalList';
-import IncomeSet from './components/Incomeset';
+//import IncomeList from './pages/IncomeList'; 
+//import EditIncome from './pages/EditIncome'; 
+//import Home from './pages/Home'; 
+//import GoalForm from './components/GoalForm';
+//import GoalList from './components/GoalList';
+//import IncomeSet from './components/Incomeset';
 
 function App() {
   return (
+
     <Router>
       <div>
       <Header /> 
+      
         <Routes>
           <Route path="/" element={< Register/>} />
-          <Route path="/dashboard" element={< Dashboard/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> 
+          
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+/*
+<Header /> 
+<Route path="/dashboard" element={< Dashboard/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/income-list" element={<IncomeList />} /> 
@@ -27,11 +43,4 @@ function App() {
           <Route path="/goals/edit/:id" element={<GoalForm />} />
           <Route path="/goals" element={<GoalList />} />
           <Route path="/incomeset" element={<IncomeSet />} />
-          
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+          */
