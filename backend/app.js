529330445a4947870bb9db66becdae29267c7634
A.R.S.A.Wimalarathna <it22207418@my.sliit.lk>
2024-08-24 21:34:55 +0530

    add routes to app.js according to the finance guard branch and intalled related things

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const axios = require('axios');
 //login 
 const userRoutes = require('./routes/userRoutes');
 
@@ -12,10 +12,17 @@ const router=require("./routes/MeetAssistRoutes");
 const vehicleRoutes = require('./routes/vehicleRoutes');
 const reminderRoutes = require('./routes/reminderRoutes');
 const serviceStationRoutes = require('./routes/serviceStationRoutes');
+const incomeRoutes = require('./routes/incomeRoutes');
+const goalRoutes = require('./routes/goalRoutes');
+
+
 
 
 const app = express();
 const bodyParser = require('body-parser');
+
+//require('./scheduler');
+require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -31,6 +38,54 @@ app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/reminders', reminderRoutes);
 app.use('/api/service-stations', serviceStationRoutes);
 
+
+//finance guard
+app.use('/income', incomeRoutes);
+app.use('/goals', goalRoutes); 
+//finance-guard
+/*app.get('/api/rates', async (req, res) => {
+  try {
+    
+    const goldResponse = await axios.get('https://www.alphavantage.co/query', {
+      params: {
+        function: 'TIME_SERIES_DAILY',
+        symbol: 'XAUUSD',  
+        apikey: process.env.ALPHAVANTAGE_API_KEY,
+      },
+    });
+
+   
+    const silverResponse = await axios.get('https://www.alphavantage.co/query', {
+      params: {
+        function: 'TIME_SERIES_DAILY',
+        symbol: 'XAGUSD',  
+        apikey: process.env.ALPHAVANTAGE_API_KEY,
+      },
+    });
+
+   
+    const latestGoldDate = Object.keys(goldResponse.data['Time Series (Daily)'])[0];
+    const latestGoldPrice = goldResponse.data['Time Series (Daily)'][latestGoldDate]['4. close'];
+    const goldRatePerGram = parseFloat(latestGoldPrice) / 31.1035;
+
+   
+    const latestSilverDate = Object.keys(silverResponse.data['Time Series (Daily)'])[0];
+    const latestSilverPrice = silverResponse.data['Time Series (Daily)'][latestSilverDate]['4. close'];
+    const silverRatePerGram = parseFloat(latestSilverPrice) / 28.3495;
+
+    
+    res.json({
+      goldRatePerGram: goldRatePerGram.toFixed(2),
+      silverRatePerGram: silverRatePerGram.toFixed(2),
+    });
+  } catch (error) {
+    console.error('Error fetching rates:', error);
+    res.status(500).json({ error: 'Failed to fetch rates' });
+  }
+});
+
+
+*/
 
 mongoose.connect("mongodb+srv://chamilasewmini2:CScs2436@portfolio.kr18ftg.mongodb.net/")
   .then(() => console.log('Connected to MongoDB'))
