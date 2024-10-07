957afbaec615b45b504e7e384ec2f138acbc341a
A.R.S.A.Wimalarathna <it22207418@my.sliit.lk>
2024-10-07 15:24:05 +0530

    add front end of finance gauide , and remove the event minder

--- frontend/src/components/MeetAssist/AddNewMeeting/AddNewMeeting.js
+++ frontend/src/components/MeetAssist/AddNewMeeting/AddNewMeeting.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import MeetNav from '../MeetNav/MeetNav';
+import MeetNav from '../../Nav/Nav';
 import Footer from '../../Footer/Footer';
 
 
